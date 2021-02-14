011006b98bd5f30c68853e21de937239e355c3c5
Bluenarysun <tee.emilond@gmail.com>
2021-02-14 15:47:14 +0700

    Footer done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let Countdown = require("countdown-js");
+
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from "swiper";
 
@@ -19,15 +21,59 @@ const swiper = new Swiper(".swiper-container", {
       return '<span class="' + className + '"></span>';
     },
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
 });
+
+var scrollableElement = document.body;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+scrollableElement.addEventListener("wheel", checkScrollDirection);
+
+function checkScrollDirection(event) {
+  if (checkScrollDirectionIsUp(event)) {
+    console.log("up");
+  } else {
+    console.log("down");
+  }
+}
+
+function checkScrollDirectionIsUp(event) {
+  if (event.wheelDelta) {
+    // console.log(event.wheelDelta);
+    // return event.wheelDelta > 0;
+    // scrollIfNeeded();
+  }
+  return event.deltaY < 0;
+}
+
+const progress = document.querySelector(".time-1");
+const final = parseInt(progress.getAttribute("data-done"));
+
+const duration = 1000;
+let start;
+
+const step = (ts) => {
+  if (!start) {
+    start = ts;
+  }
+  // get the time passed as a fraction of total duration
+  let prog = (ts - start) / duration;
+  progress.textContent = Math.floor(prog * final);
+  // if (prog < 1) {
+  //   // if not done, request another frame
+  //   requestAnimationFrame(step);
+  // }
+};
+
+
+  
+    
+function scrollIfNeeded(element, container) {
+  if (element.offsetTop < container.scrollTop) {
+    container.scrollTop = element.offsetTop;
+  } else {
+    const offsetBottom = element.offsetTop + element.offsetHeight;
+    const scrollBottom = container.scrollTop + container.offsetHeight;
+    if (offsetBottom > scrollBottom) {
+      container.scrollTop = offsetBottom - container.offsetHeight;
+    }
+  }
+}
