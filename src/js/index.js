7d1aa266eda37cf9135c7e23321c993b9fcc8e77
Bluenarysun <tee.emilond@gmail.com>
2021-02-15 10:27:12 +0700

    JS counter new

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 "use strict";
 
+import Swiper, { Navigation, Pagination } from "swiper";
 import './../scss/index.scss';
 import WOW from "wow.js/dist/wow.js";
 import imagesLoaded from "imagesloaded";
+import './countdown.js'
 
-var Isotope = require('isotope-layout');
-let Countdown = require("countdown-js");
+let Isotope = require('isotope-layout');
+
 
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from "swiper";
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
@@ -28,58 +29,6 @@ const swiper = new Swiper(".swiper-container", {
   },
 });
 
-var scrollableElement = document.body;
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-scrollableElement.addEventListener("wheel", checkScrollDirection);
-
-function checkScrollDirection(event) {
-  if (checkScrollDirectionIsUp(event)) {
-    // console.log("up");
-  } else {
-    // console.log("down");
-  }
-}
-
-function checkScrollDirectionIsUp(event) {
-  if (event.wheelDelta) {
-    // console.log(event.wheelDelta);
-    // return event.wheelDelta > 0;
-    // scrollIfNeeded();
-  }
-  return event.deltaY < 0;
-}
-
-const progress = document.querySelector(".time-1");
-const final = parseInt(progress.getAttribute("data-done"));
-
-const duration = 1000;
-let start;
-
-const step = (ts) => {
-  if (!start) {
-    start = ts;
-  }
-  // get the time passed as a fraction of total duration
-  let prog = (ts - start) / duration;
-  progress.textContent = Math.floor(prog * final);
-  // if (prog < 1) {
-  //   // if not done, request another frame
-  //   requestAnimationFrame(step);
-  // }
-};
-
-    
-function scrollIfNeeded(element, container) {
-  if (element.offsetTop < container.scrollTop) {
-    container.scrollTop = element.offsetTop;
-  } else {
-    const offsetBottom = element.offsetTop + element.offsetHeight;
-    const scrollBottom = container.scrollTop + container.offsetHeight;
-    if (offsetBottom > scrollBottom) {
-      container.scrollTop = offsetBottom - container.offsetHeight;
-    }
-  }
-}
 
 // WOW
 var wow = new WOW({
